a0ef8a0d3803c31a668bf0e0a22914f1772680f4
nadim249 <mahmoudnadim47@gmail.com>
2026-01-02 16:16:03 +0600

    Design:dashbpardpage

---
+++ Admin/View/AdminDash/dashboard.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Public/CSS/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <title>Dashboard</title>
+</head>
+<body id="page-dashboard">
+    <div class="sidebar">
+        <div class="logo">
+            <i class="fa-solid fa-building fa-2x"></i>
+            <h2>EstateMgr</h2>
+        </div>
+        <ul class="menu">
+            <li>
+                <a href="dashboard.php">
+                    <i class="fa-solid fa-gauge"></i> 
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="properties.php">
+                    <i class="fa-solid fa-house"></i> 
+                    <span>Properties</span>
+                </a>
+            </li>
+            <li>
+                <a href="approvals.php">
+                    <i class="fa-solid fa-clipboard-check"></i>
+                     <span>Approvals</span>
+                    </a>
+            </li>
+            <li>
+                <a href="transactions.php">
+                    <i class="fa-solid fa-money-bill-wave"></i> 
+                    <span>Transactions</span>
+                </a>
+            </li>
+            <li>
+                <a href="agents.php">
+                    <i class="fa-solid fa-user-tie"></i> 
+                    <span>Agents</span>
+                </a>
+            </li>
+            <li>
+                <a href="users.php">
+                    <i class="fa-solid fa-users"></i> 
+                    <span>Users</span>
+                </a>
+            </li>
+            <li class="logout-btn">
+                <a href="#"><i class="fa-solid fa-right-from-bracket"></i> <span>Logout</span></a>
+            </li>
+        </ul>
+    </div>
+    <main class="main-content">
+        <header>
+            <div class="header-title">
+                <h1>Dashboard Overview</h1>
+            </div>
+                <div class="user-wrapper">
+                    <i class="fa-duotone fa-solid fa-user user-img"></i>
+                    <div>
+                    <h4>Admin</h4>
+                    <small>Super Admin</small>
+                    </div>
+                </div>
+        </header>
+        <div class="cards-grid" id="stats-container">
+                    <div class="single-card">
+                        <div>
+                            <h1>54</h1>
+                                <span>Total properties</span>
+                        </div>
+                    <div>
+                        <span class="fa-solid fa-house" id="logo-card"></span>
+                    </div>
+                    </div>
+        </div>
+
+        <div class="table-responsive">
+            <h3>Recent Listings</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Property Title</td>
+                        <td>Price</td>
+                        <td>Status</td>
+                        <td>Agent</td>
+                    </tr>
+                </thead>
+                <tbody id="property-body">
+                    <tr>
+                        <td>Dhanmodi house</td>
+                        <td>32000000</td>
+                        <td><span class="status"></span>sale</td>
+                        <td>Rijon</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+    </main>
+</body>
+</html>
--- Admin/View/Auth/login.php
+++ Admin/View/Auth/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../../Public/Css/styles.css">
+    <link rel="stylesheet" href="../../Public/CSS/styles.css">
 </head>
 <body>
     <body id="page-login">
