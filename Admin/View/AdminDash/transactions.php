55fc432b2a5e5a752013eb30a0f7617205abcda9
nadim249 <mahmoudnadim47@gmail.com>
2026-01-03 14:44:32 +0600

    Design:agents and transactions and user addd

---
+++ Admin/View/AdminDash/transactions.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Transactions | EstateMgr</title>
+    <link rel="stylesheet" href="../../Public/CSS/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<body id="page-transactions">
+    <div class="sidebar">
+        <div class="logo">
+            <i class="fa-solid fa-building fa-2x"></i>
+            <h2><a href="dashboard.php">
+            EstateMgr</h2>
+        </div>
+        <ul class="menu">
+            <li>
+                <a href="dashboard.php">
+                    <i class="fa-solid fa-gauge"></i> 
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="propertiesdata.php">
+                    <i class="fa-solid fa-house"></i> 
+                    <span>Properties</span>
+                </a>
+            </li>
+            <li>
+                <a href="approvals.php">
+                    <i class="fa-solid fa-clipboard-check"></i>
+                     <span>Approvals</span>
+                    </a>
+            </li>
+            <li>
+                <a href="transactions.php">
+                    <i class="fa-solid fa-money-bill-wave"></i> 
+                    <span>Transactions</span>
+                </a>
+            </li>
+            <li>
+                <a href="agents.php">
+                    <i class="fa-solid fa-user-tie"></i> 
+                    <span>Agents</span>
+                </a>
+            </li>
+            <li>
+                <a href="users.php">
+                    <i class="fa-solid fa-users"></i> 
+                    <span>Users</span>
+                </a>
+            </li>
+            <li class="logout-btn">
+                <a href="#"><i class="fa-solid fa-right-from-bracket"></i> <span>Logout</span></a>
+            </li>
+        </ul>
+    </div>
+    <main class="main-content">
+        <header><div class="header-title"><h1>Financial Transactions</h1></div></header>
+        <div class="table-responsive">
+            <table><thead><tr><td>Trx ID</td><td>Property</td><td>Buyer</td><td>Token Paid</td><td>Balance Due</td><td>Status</td><td>Action</td></tr></thead><tbody id="transaction-list"></tbody></table>
+        </div>
+    </main>
+
+</body>
+</html>
